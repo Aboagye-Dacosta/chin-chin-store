2a7d18aa62ea547cc4def253f2735fc8bf711f73
Solomon Aboagye <solomon.aboagye@amalitech.com>
2025-08-19 02:29:24 +0000

    chore: fix path resolving issues

--- components/admin-stats.tsx
+++ components/admin-stats.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Package, Users, ShoppingCart, DollarSign } from 'lucide-react'
 
