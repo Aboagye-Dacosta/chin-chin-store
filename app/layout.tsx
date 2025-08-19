51962993b3d2d68889620289e20f7fd5debeb29f
Solomon Aboagye <solomon.aboagye@amalitech.com>
2025-08-19 01:38:47 +0000

    chore: remove unused files

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,6 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 import { Provider } from "@/provider";
-import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
-import { extractRouterConfig } from "uploadthing/server";
-import { ourFileRouter } from "@/app/api/uploadthing/core";
 import { AppThemeFloatingActionButton } from "@/components/app-theme-floating-action-button";
 import { ThemeProvider } from "next-themes";
 import { cn } from "@/lib/utils";
@@ -24,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(inter.className, "h-screen overflow-hidden")} >
+      <body className={cn(inter.className, "h-screen overflow-hidden")}>
         <ThemeProvider>
           <Provider>
             {children}
@@ -32,7 +29,6 @@ export default function RootLayout({
             <AppThemeFloatingActionButton />
           </Provider>
         </ThemeProvider>
-        <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
       </body>
     </html>
   );
