2afec45db164ad54f61b55e930e4da821b9487f0
Solomon Aboagye <solomon.aboagye@amalitech.com>
2025-08-21 12:55:02 +0000

    chore: add payment and order page for checkout to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/admin(.*)', '/cart/checkout(.*)' , "/orders(.*)"])
+const isProtectedRoute = createRouteMatcher(['/admin(.*)', '/cart/checkout/order(.*)','/cart/checkout/payment(.*)' , "/orders(.*)"])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
